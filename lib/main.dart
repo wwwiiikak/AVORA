9f3934d499797a5edc004df719e2d4b1d00d5c44
AVORA <sr8656645@gmail.com>
2026-09-11 23:00:27 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -194,7 +194,7 @@ class _BookingPageState extends State<BookingPage> {
         'order_number': resi,
         'pickup_address': pickup.text.trim(),
         'destination_address': destination.text.trim(),
-        'package_type': 'package', 'Paket' : type.text.trim(),
+        'package_type': 'Paket',
         'weight_kg': weight,
         'vehicle_type': vehicle,
         'service_type': service,
